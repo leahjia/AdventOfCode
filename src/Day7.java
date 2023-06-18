3b92d0445aff451423f252b02983df5a03dcee97
Leah Jia <yuwenj3@uw.edu>
2023-06-18 15:34:10 -0700

    day7

--- src/Day7.java
+++ src/Day7.java
@@ -4,6 +4,35 @@ import java.util.*;
 
 public class Day7 {
     
+    // Tree structure storing all directories & files
+    private static class Tree {
+        TreeNode root;
+        
+        private Tree() {
+            this.root = new TreeNode("root");
+        }
+    }
+    
+    // Node storing a single directory or file
+    private static class TreeNode {
+        int val;
+        String name;
+        Map<String, TreeNode> branch;
+        
+        // name is only for debugging purposes
+        private TreeNode(String name) {
+            this.name = name;
+            this.val = 0;
+            this.branch = new HashMap<>();
+        }
+    }
+    
+    // part 1 - list of small-enough directories
+    static List<TreeNode> directories = new ArrayList<>();
+    
+    // part 2 target storage needed
+    static int target;
+    
     public static void main(String[] args) throws FileNotFoundException {
         Scanner in = new Scanner(new FileReader("input/day7.txt"));
         in.nextLine();
@@ -17,42 +46,12 @@ public class Day7 {
         for (TreeNode dir : directories) count += dir.val;
         System.out.println("Part 1: " + count);
         
-        // System.out.println("\n Printing Tree: ");
-        // dfs(tree.root);
-        
         // part 2
         target = 30000000 - (70000000 - tree.root.val);
         System.out.println("Part 2: " + dfs(tree.root, tree.root.val));
     }
     
-    static int target;
-    
-    // part 2 - run search
-    private static int dfs(TreeNode root, int curr) {
-        // not a directory or not large enough
-        if (root.branch.isEmpty() || root.val < target) return Integer.MAX_VALUE;
-        
-        // update current minimum
-        curr = Math.min(curr, root.val);
-        
-        // run dfs on each branch
-        for (TreeNode node : root.branch.values()) {
-            curr = Math.min(curr, dfs(node, curr));
-        }
-        return curr; // ~5756764
-    }
-    
-    // Testing tree structure
-    private static void dfs(TreeNode root) {
-        System.out.println(root.name + ": " + root.val);
-        for (TreeNode node : root.branch.values()) {
-            if (!node.branch.isEmpty()) dfs(node);
-        }
-    }
-    
     // part 1 - construct tree & find directories
-    static List<TreeNode> directories = new ArrayList<>();
-    
     private static int traverse(Scanner in, TreeNode root) {
         while (in.hasNext()) {
             String str = in.nextLine();
@@ -88,27 +87,16 @@ public class Day7 {
         return root.val; // ~1297683
     }
     
-    
-    // Tree structure storing all directories & files
-    private static class Tree {
-        TreeNode root;
-        
-        private Tree() {
-            this.root = new TreeNode("root");
+    // part 2 - run search
+    private static int dfs(TreeNode root, int curr) {
+        // not a directory or not large enough
+        if (root.branch.isEmpty() || root.val < target) return Integer.MAX_VALUE;
+        // update current minimum
+        curr = Math.min(curr, root.val);
+        // run dfs on each branch
+        for (TreeNode node : root.branch.values()) {
+            curr = Math.min(curr, dfs(node, curr));
         }
+        return curr; // ~5756764
     }
-    
-    // Node storing a single directory or file
-    private static class TreeNode {
-        int val;
-        String name;
-        Map<String, TreeNode> branch;
-        
-        private TreeNode(String name) {
-            this.name = name;
-            this.val = 0;
-            this.branch = new HashMap<>();
-        }
-    }
-    
 }
