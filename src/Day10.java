4131a53d082bc208576561e97c7f2de9c674e65d
Leah Jia <yuwenj3@uw.edu>
2023-06-23 09:55:06 -0700

    10-2

--- src/Day10.java
+++ src/Day10.java
@@ -6,40 +6,44 @@ public class Day10 {
         Scanner in = new Scanner(new FileReader("input/day10.txt"));
         List<String> commands = new ArrayList<>();
         while (in.hasNext()) commands.add(in.nextLine());
+        
         System.out.println("part 1: " + CPU(commands)); // 16880
         System.out.println("part 2: ");
         CRT(commands);
     }
     
+    static String line;
+    static int drawing;
+    static int X;
+    
     private static void CRT(List<String> list) {
-        int X = 1;
-        int drawing = 0;
-        String line = "#";
+        X = 1;
+        drawing = 0;
+        line = "#";
         for (String s : list) {
             String[] curr = s.split(" ");
-            drawing++;
-            if (drawing == 40) {
-                System.out.println(line);
-                line = "";
-                drawing = 0;
-            }
-            line += (X - 1 <= drawing && drawing <= X + 1) ? "#" : ".";
+            checkPosition();
             if (curr[0].equals("addx")) {
                 X += Integer.parseInt(curr[1]);
-                drawing++;
-                if (drawing == 40) {
-                    System.out.println(line);
-                    line = "";
-                    drawing = 0;
-                }
-                line += (X - 1 <= drawing && drawing <= X + 1) ? "#" : ".";
+                checkPosition();
             }
         }
     }
     
+    // for CRT
+    private static void checkPosition() {
+        drawing++;
+        if (drawing == 40) {
+            System.out.println(line);
+            line = "";
+            drawing = 0;
+        }
+        line += (X - 1 <= drawing && drawing <= X + 1) ? "#" : ".";
+    }
+    
     private static int CPU(List<String> list) {
         int cycle = 1;
-        int X = 1;
+        X = 1;
         int sum = 0;
         for (String s : list) {
             String[] curr = s.split(" ");
