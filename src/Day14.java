dcef2f71a23b340af902d4e9d82ca4177fc64ce4
Leah Jia <yuwenj3@uw.edu>
2023-07-15 01:07:49 -0700

    Day14-1 update

--- src/Day14.java
+++ src/Day14.java
@@ -4,34 +4,49 @@ import java.util.*;
 public class Day14 {
     static char[][] grid;
     static int count;
-//    static int leftBound;
     
     public static void main(String[] args) throws FileNotFoundException {
         Scanner in = new Scanner(new FileReader("input/day14.txt"));
-        List<List<int[]>> input = new ArrayList<>();
+        List<String> tokens = new ArrayList<>();
+        while (in.hasNext()) tokens.add(in.nextLine());
+        
         int row = 0;
         int col = 0;
         int leftBound = Integer.MAX_VALUE;
-        while (in.hasNext()) {
-            String[] path = in.nextLine().split(" -> ");
+        List<List<int[]>> input = new ArrayList<>();
+        
+        for (String line : tokens) {
+            String[] path = line.split(" -> ");
             List<int[]> segments = new ArrayList<>();
             for (String str : path) {
-                String[] pair = str.split(",");
-                int[] xy = new int[]{Integer.parseInt(pair[0]), Integer.parseInt(pair[1])};
-                col = Math.max(col, xy[0]);
-                leftBound = Math.min(leftBound, xy[0]);
-                row = Math.max(row, xy[1]);
-                segments.add(xy);
+                int[] pair = new int[]{Integer.parseInt(str.split(",")[0]), Integer.parseInt(str.split(",")[1])};
+                row = Math.max(row, pair[1]);
+                col = Math.max(col, pair[0]);
+                leftBound = Math.min(leftBound, pair[0]);
+                segments.add(pair);
             }
             input.add(segments);
         }
         
+        for (List<int[]> seg : input) {
+            for (int[] pair : seg) {
+                pair[0] -= leftBound;
+            }
+        }
+        for (List<int[]> seg : input) {
+            for (int[] pair : seg) {
+                System.out.println(Arrays.toString(pair));
+            }
+        }
+        
+        int start = 500 - leftBound;
+        
         col -= leftBound;
-        getGrid(input, row, col, leftBound);
+        getGrid(input, row, col, start);
         
         // part 1
         count = 0;
-        sandFall(row, col, leftBound);
+        sandFall(row, col, start);
         print(grid);
         System.out.println("Count: " + count); // ~799
         
@@ -44,19 +59,19 @@ public class Day14 {
 //        System.out.println(count);
     }
     
-    private static void getGrid(List<List<int[]>> input, int row, int col, int leftBound) {
+    private static void getGrid(List<List<int[]>> input, int row, int col, int start) {
         grid = new char[row + 1][col + 1];
         for (char[] r : grid) {
             Arrays.fill(r, '.');
         }
-        grid[0][500 - leftBound] = '+';
+        grid[0][start] = '+';
         
         for (List<int[]> path : input) {
             for (int i = 0; i < path.size() - 1; i++) {
                 int[] a = path.get(i);
                 int[] b = path.get(i + 1);
                 for (int r = Math.min(a[1], b[1]); r <= Math.max(a[1], b[1]); r++) {
-                    for (int c = Math.min(a[0] - leftBound, b[0] - leftBound); c <= Math.max(a[0] - leftBound, b[0] - leftBound); c++) {
+                    for (int c = Math.min(a[0], b[0]); c <= Math.max(a[0], b[0]); c++) {
                         grid[r][c] = '#';
                     }
                 }
@@ -64,10 +79,10 @@ public class Day14 {
         }
     }
     
-    private static void sandFall(int row, int col, int leftBound) {
+    private static void sandFall(int row, int col, int start) {
         while (true) {
             int r = 0;
-            int c = 500 - leftBound;
+            int c = start;
             
             // straight down || down-left || down-right
             while (grid[r + 1][c] == '.' || grid[r + 1][c - 1] == '.' || grid[r + 1][c + 1] == '.') {
