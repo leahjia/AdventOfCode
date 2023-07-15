13cac950c40c60aba330d1cee32bdb6c7c281ebd
Leah Jia <yuwenj3@uw.edu>
2023-07-15 14:45:32 -0700

    Day14-2

--- src/Day14.java
+++ src/Day14.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Day14 {
     static char[][] grid;
-    static int count;
+    static List<List<int[]>> input;
     
     public static void main(String[] args) throws FileNotFoundException {
         Scanner in = new Scanner(new FileReader("input/day14.txt"));
@@ -13,7 +13,7 @@ public class Day14 {
         int row = 0;
         int col = 0;
         int leftBound = Integer.MAX_VALUE;
-        List<List<int[]>> input = new ArrayList<>();
+        input = new ArrayList<>();
         
         for (String line : tokens) {
             String[] path = line.split(" -> ");
@@ -33,45 +33,35 @@ public class Day14 {
                 pair[0] -= leftBound;
             }
         }
-        for (List<int[]> seg : input) {
-            for (int[] pair : seg) {
-                System.out.println(Arrays.toString(pair));
-            }
-        }
+        col -= leftBound;
         
         int start = 500 - leftBound;
         
-        col -= leftBound;
-        getGrid(input, row, col, start);
+        // part 1 - no floor
+        getGrid(row, col, start, 0);
+        System.out.println("Part I count: " + getSand(start)); // ~799
         
-        // part 1
-        count = 0;
-        sandFall(row, col, start);
-        print(grid);
-        System.out.println("Count: " + count); // ~799
-        
-        // part 2
-//        getGrid(input, row + 2, col + 10, leftBound);
-//        Arrays.fill(grid[row + 2], '#');
-//        count = 0;
-//        sandFall(row + 2, col + 10, leftBound);
-//        print(grid);
-//        System.out.println(count);
+        // part 2 - add floor 2 levels down
+        int shift = 150;
+        getGrid(row + 2, col, start + shift, shift);
+        Arrays.fill(grid[row + 2], '#');
+        System.out.println("Part II count: " + getSand(start + shift)); // ~29076
     }
     
-    private static void getGrid(List<List<int[]>> input, int row, int col, int start) {
-        grid = new char[row + 1][col + 1];
+    private static void getGrid(int row, int col, int src, int shift) {
+        grid = new char[row + 1][col + shift * 3 + 1];
         for (char[] r : grid) {
             Arrays.fill(r, '.');
         }
-        grid[0][start] = '+';
+        grid[0][src] = '+';
         
+        // label all rocks
         for (List<int[]> path : input) {
             for (int i = 0; i < path.size() - 1; i++) {
                 int[] a = path.get(i);
                 int[] b = path.get(i + 1);
                 for (int r = Math.min(a[1], b[1]); r <= Math.max(a[1], b[1]); r++) {
-                    for (int c = Math.min(a[0], b[0]); c <= Math.max(a[0], b[0]); c++) {
+                    for (int c = Math.min(a[0], b[0]) + shift; c <= Math.max(a[0], b[0]) + shift; c++) {
                         grid[r][c] = '#';
                     }
                 }
@@ -79,7 +69,10 @@ public class Day14 {
         }
     }
     
-    private static void sandFall(int row, int col, int start) {
+    private static int getSand(int start) {
+        int row = grid.length;
+        int col = grid[0].length;
+        int count = 0;
         while (true) {
             int r = 0;
             int c = start;
@@ -87,28 +80,24 @@ public class Day14 {
             // straight down || down-left || down-right
             while (grid[r + 1][c] == '.' || grid[r + 1][c - 1] == '.' || grid[r + 1][c + 1] == '.') {
                 if (grid[r + 1][c] != '.') {
+                    // go left/right if straight down is blocked
                     c += grid[r + 1][c - 1] == '.' ? -1 : 1;
                 }
                 r++;
-                if (r >= row || c <= 0 || c > col) {
-                    break;
+                
+                // hits empty floor || left/right edge
+                if (r == row - 1 || c == 0 || c == col - 1) {
+                    return count;
                 }
             }
             
-            // left/right edge || empty floor
-            if (c == 0 || c == col || r == row) {
-                break;
-            } else {
-                count++;
-            }
             grid[r][c] = 'O';
-        }
-    }
-    
-    private static void print(char[][] grid) {
-        int i = 0;
-        for (char[] chars : grid) {
-            System.out.println(i++ + " " + Arrays.toString(chars));
+            count++;
+            
+            // if source is finally blocked
+            if (grid[0][start] == 'O') {
+                return count;
+            }
         }
     }
 }
