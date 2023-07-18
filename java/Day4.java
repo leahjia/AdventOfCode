b0b9ea1f029efb1d42a95db5641880c4d38bf231
Leah Jia <yuwenj3@uw.edu>
2023-07-17 20:49:43 -0700

    .

--- java/Day4.java
+++ java/Day4.java
@@ -2,28 +2,18 @@ import java.io.*;
 import java.util.*;
 
 public class Day4 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws FileNotFoundException {
         Scanner in = new Scanner(new FileReader("input/day4.txt"));
-        System.out.println(overlap(in));
-    }
-    
-    private static int overlap(Scanner in) throws IOException {
-        int res = 0;
+        int part1 = 0;
+        int part2 = 0;
         while (in.hasNextLine()) {
             String[] line = in.nextLine().split("[,-]");
-            int[][] pair = {{Integer.parseInt(line[0]), Integer.parseInt(line[1])},
-                    {Integer.parseInt(line[2]), Integer.parseInt(line[3])}};
-            // int[] line = Arrays.stream(in.readLine().split("[,-]")).mapToInt(Integer::parseInt).toArray();
-          
+            int[][] pair = {{Integer.parseInt(line[0]), Integer.parseInt(line[1])}, {Integer.parseInt(line[2]), Integer.parseInt(line[3])}};
             // start in ASC, end in DESC
             Arrays.sort(pair, (a, b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
-            
-            // part 1
-            // if (pair[0][1] >= pair[1][1]) res++;
-            
-            // part 2
-            if (pair[0][1] >= pair[1][0]) res++;
+            if (pair[0][1] >= pair[1][1]) part1++;
+            if (pair[0][1] >= pair[1][0]) part2++;
         }
-        return res;
+        System.out.println("Part 1: " + part1 + "\nPart 2: " + part2);
     }
 }
