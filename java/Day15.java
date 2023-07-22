3f52c363ab2d2670f752cc77d07c63962e9c327e
Leah Jia <yuwenj3@uw.edu>
2023-07-21 19:07:21 -0700

    why

--- java/Day15.java
+++ java/Day15.java
@@ -1,106 +1,72 @@
 import java.io.*;
 import java.util.*;
 import java.util.regex.*;
-import java.util.stream.Collectors;
 
 public class Day15 {
     
     static int left = Integer.MAX_VALUE;
     static int right = Integer.MIN_VALUE;
-    static List<int[][]> pairs;
-    static Map<Integer, Set<Integer>> map;
+    static Set<Point> set;
+    static List<Point[]> pairs;
     static final int BOUND = 4000000;
     
     public static void main(String[] args) throws FileNotFoundException {
         Scanner in = new Scanner(new FileReader("input/day15.txt"));
         pairs = new ArrayList<>();
-        int upper = Integer.MIN_VALUE;
-        int lower = Integer.MAX_VALUE;
-        Pattern pat = Pattern.compile("x=(-?\\d+),\\s*y=(-?\\d+)");
+        parser(in);
         
-        while (in.hasNext()) {
-            Matcher mat = pat.matcher(in.nextLine());
-            
-            mat.find();
-            int[] sensor = new int[]{Integer.parseInt(mat.group(1)), Integer.parseInt(mat.group(2))};
-            mat.find();
-            int[] beacon = new int[]{Integer.parseInt(mat.group(1)), Integer.parseInt(mat.group(2))};
-            pairs.add(new int[][]{sensor, beacon});
-
-//            lower = Math.min(lower, Math.min(sensor[1], beacon[1]));
-//            upper = Math.max(upper, Math.max(sensor[1], beacon[1]));
-            int dist = getDist(sensor, beacon);
-            lower = Math.min(lower, sensor[1] - dist + 1);
-            upper = Math.max(upper, sensor[1] + dist + 1);
-            left = Math.min(left, sensor[0] - dist + 1);
-            right = Math.max(right, sensor[0] + dist);
-        }
+        // part 1 - where points cannot exist
+        // System.out.println("Count: " + getRange(pairs, 2000000)); // 5809294
         
-        map = new HashMap<>();
-//        System.out.println("Count: " + getRange(pairs, 2000000)); // 5809294
-        
+        // part 2
         long startTime = System.currentTimeMillis();
-        findPoints(4000000);
+        set = new TreeSet<>(new PointComparator());
+        findPoints();
+        System.out.println(set.size());
+        for (Point p : set) {
+            System.out.println(p.x + ", " + p.y);
+        }
         System.out.println("Execution time: " + (System.currentTimeMillis() - startTime) + "ms");
-        System.out.println("Point? " + map);
     }
     
-    private static void findPoints(int bound) {
+    private static void findPoints() {
         // narrow down candidate list
-//        List<int[][]> clone = pairs.stream().map(array -> Arrays.stream(array).map(int[]::clone).toArray(int[][]::new)).collect(Collectors.toList());
-
-//        for (int i = clone.size() - 1; i >= 0; i++) {
-//            Sensor a = new Sensor(clone.get(i));
-//            for (int j = clone.size() - 1; j >= 0; j++) {
-//                Sensor b = new Sensor(clone.get(j));
-//                if (getDist(a.pt, b.pt) * 2 <= a.range) {
-//                    pairs.remove(j);
-//                }
-//                if (getDist(a.pt, b.pt) * 2 <= b.range) {
-//                    pairs.remove(i);
-//                }
-//            }
-//        }
-        for (int[][] pair : pairs) {
-            int[] sensor = pair[0];
-            int[] beacon = pair[1];
+        for (Point[] pair : pairs) {
+            Point sensor = pair[0];
+            Point beacon = pair[1];
             int dist = getDist(sensor, beacon);
-            int row = sensor[1];
+            int row = sensor.y;
             int y = -1;
             
-            // search through the parameter
-            int len = sensor[0] + dist + 1;
-            for (int x = sensor[0] - dist - 1; x <= len; x++) {
-                y += x < len / 2 ? 1 : -1;
-                if (0 <= x && x <= bound) {
-                    Set<Integer> set = map.getOrDefault(x, new HashSet<>());
-                    if (0 <= row - y && row - y <= bound) set.add(row - y);
-                    if (0 <= row + y && row + y <= bound) set.add(row + y);
-                    if (!set.isEmpty()) map.put(x, set);
+            // search around the parameter
+            int width = sensor.x + dist + 1;
+            for (int x = sensor.x - dist - 1; x <= width; x++) {
+                // y increase on first half, decrease second half
+                y += x < width / 2 ? 1 : -1;
+                if (0 <= x && x <= BOUND) {
+                    if (0 <= row - y && row - y <= BOUND) set.add(new Point(x, row - y));
+                    if (0 <= row + y && row + y <= BOUND) set.add(new Point(x, row + y));
                 }
             }
             checkPoints();
-            if (map.size() == 1) {
-                System.out.println("returning");
-                return;
-            }
         }
     }
     
     private static void checkPoints() {
-        for (int x : map.keySet()) {
-            Set<Integer> set = map.get(x);
-            set.removeIf(y -> isInRange(x, y));
-            map.put(x, set);
+        Set<Point> remove = new TreeSet<>(new PointComparator());
+        for (Point pt : set) {
+            if (isInRange(pt.x, pt.y)) {
+                remove.add(pt);
+            }
         }
-        map.entrySet().removeIf(e -> e.getValue() != null && (e.getValue()).isEmpty());
+        set.removeAll(remove);
     }
     
     private static boolean isInRange(int x, int y) {
-        int[] point = new int[]{x, y};
-        for (int[][] pair : pairs) {
-            int[] sensor = pair[0];
-            int[] beacon = pair[1];
+        Point point = new Point(x, y);
+        for (Point[] pair : pairs) {
+            Point sensor = pair[0];
+            Point beacon = pair[1];
             if (getDist(point, sensor) <= getDist(sensor, beacon)) {
                 return true;
             }
@@ -108,25 +74,61 @@ public class Day15 {
         return false;
     }
     
-    private static int getRange(List<int[][]> pairs, int row) {
+    private static Point part2(List<Point[]> pairs, int row) {
+        char[] chars = new char[BOUND + 1];
+        Arrays.fill(chars, '.');
+        
+        // applying each pair to that row
+        for (Point[] pair : pairs) {
+            Point sensor = pair[0];
+            Point beacon = pair[1];
+            int dist = getDist(sensor, beacon);
+            
+            // label sensor and beacon
+            if (sensor.y == row && 0 <= sensor.x && sensor.x <= BOUND) chars[sensor.x] = 'S';
+            if (beacon.y == row && 0 <= beacon.x && beacon.x <= BOUND) chars[beacon.x] = 'B';
+            
+            // if row is in range (sensor.y +- dist), count #
+            if (sensor.y - dist <= row && row <= sensor.y + dist) {
+                // run from left to right of that row
+                for (int i = 0; i <= BOUND; i++) {
+                    Point point = new Point(i, row);
+                    if (chars[i] == '.' && getDist(point, sensor) <= dist) {
+                        chars[i] = '#';
+                    }
+                }
+            }
+        }
+
+//        System.out.print(row + ": ");
+//        System.out.println(chars);
+        
+        for (int i = 0; i <= BOUND; i++) {
+            char ch = chars[i];
+            if (ch == '.') return new Point(i, row);
+        }
+        return null;
+    }
+    
+    private static int getRange(List<Point[]> pairs, int row) {
         char[] chars = new char[right - left];
         Arrays.fill(chars, '.');
         
         // applying each pair to that row
-        for (int[][] pair : pairs) {
-            int[] sensor = pair[0];
-            int[] beacon = pair[1];
+        for (Point[] pair : pairs) {
+            Point sensor = pair[0];
+            Point beacon = pair[1];
             int dist = getDist(sensor, beacon);
             
             // label sensor and beacon
-            if (sensor[1] == row) chars[sensor[0] - left] = 'S';
-            if (beacon[1] == row) chars[beacon[0] - left] = 'B';
+            if (sensor.y == row) chars[sensor.x - left] = 'S';
+            if (beacon.y == row) chars[beacon.x - left] = 'B';
             
             // if row is in range (sensor.y +- dist), count #
-            if (sensor[1] - dist <= row && row <= sensor[1] + dist) {
+            if (sensor.y - dist <= row && row <= sensor.y + dist) {
                 // run from left to right of that row
                 for (int i = left; i < right; i++) {
-                    int[] point = new int[]{i, row};
+                    Point point = new Point(i, row);
                     if (chars[i - left] == '.' && getDist(point, sensor) <= dist) {
                         chars[i - left] = '#';
                     }
@@ -141,7 +143,46 @@ public class Day15 {
         return count;
     }
     
-    private static int getDist(int[] a, int[] b) {
-        return Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]);
+    // parsing input file
+    private static void parser(Scanner in) {
+        Pattern pattern = Pattern.compile("x=(-?\\d+),\\s*y=(-?\\d+)");
+        while (in.hasNext()) {
+            Matcher matcher = pattern.matcher(in.nextLine());
+            matcher.find();
+            Point sensor = new Point(Integer.parseInt(matcher.group(1)), Integer.parseInt(matcher.group(2)));
+            matcher.find();
+            Point beacon = new Point(Integer.parseInt(matcher.group(1)), Integer.parseInt(matcher.group(2)));
+            pairs.add(new Point[]{sensor, beacon});
+            
+            int dist = getDist(sensor, beacon);
+            left = Math.min(left, sensor.x - dist + 1);
+            right = Math.max(right, sensor.x + dist);
+        }
+    }
+    
+    private static int getDist(Point a, Point b) {
+        return Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
+    }
+    
+    static class Point {
+        int x;
+        int y;
+        
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+        
+        boolean equals(Point other) {
+            return this.x == other.x && this.y == other.y;
+        }
+        
+    }
+    
+    static class PointComparator implements Comparator<Point> {
+        @Override
+        public int compare(Point a, Point b) {
+            return a.x == b.x ? Integer.compare(a.y, b.y) : Integer.compare(a.x, b.x);
+        }
     }
 }
