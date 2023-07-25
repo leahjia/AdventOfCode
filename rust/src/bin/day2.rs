ab959189d47ab787a85d92eb51abab5144bebf53
Leah Jia <yuwenj3@uw.edu>
2023-07-24 22:53:04 -0700

    rsd2

--- rust/src/bin/day2.rs
+++ rust/src/bin/day2.rs
@@ -6,7 +6,7 @@ lazy_static::lazy_static! {
 
 fn main() {
     println!("{}{}", "Expected 11666, Received ", part1(&FILE));
-    // println!("{}{}", "Expected 12767, Received ", part1(&FILE));
+    println!("{}{}", "Expected 12767, Received ", part2(&FILE));
 }
 
 fn part1(input: &str) -> u16 {
@@ -17,10 +17,32 @@ fn part1(input: &str) -> u16 {
             let op = pair[0] as char;
             let me = pair[1] as char;
             let me_ascii = me as u16 - 'X' as u16;
-            me_ascii + match (op, me) {
-                ('A', 'Y') | ('B', 'Z') | ('C', 'X') => 7,
-                _ if op as u16 == me_ascii + 'A' as u16 => 4,
-                _ => 1,
+            me_ascii
+                + match (op, me) {
+                    ('A', 'Y') | ('B', 'Z') | ('C', 'X') => 7,
+                    _ if op as u16 == me_ascii + 'A' as u16 => 4,
+                    _ => 1,
+                }
+        })
+        .sum()
+}
+
+fn part2(input: &str) -> u16 {
+    input
+        .split("\n")
+        .map(|line| {
+            let pair: Vec<char> = line.split_whitespace().flat_map(|ch| ch.chars()).collect();
+            let op = pair[0] as char;
+            let outcome = pair[1] as char;
+            match (op, outcome) {
+                ('A', 'X') => 3,
+                ('B', 'X') => 1,
+                ('C', 'X') => 2,
+                (_, 'Y') => 4 + op as u16 - 'A' as u16,
+                ('A', 'Z') => 8,
+                ('B', 'Z') => 9,
+                ('C', 'Z') => 7,
+                _ => 0,
             }
         })
         .sum()
