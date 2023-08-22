519494229c3aadce2e5c781c1dbd58bfb36145d1
Leah Jia <yuwenj3@uw.edu>
2023-08-21 19:21:09 -0700

    partII isn't right

--- py/Day9.py
+++ py/Day9.py
@@ -1,49 +1,46 @@
-directions = {'R': [1, 0], 'U': [0, 1], 'L': [-1, 0], 'D': [0, -1]}
+direct = {'R': [1, 0], 'L': [-1, 0], 'U': [0, 1], 'D': [0, -1]}
 visit = {}
 
 def move(steps):
-    T, H = [[0, 0] for _ in range(2)]
-    new_set = {T[1]}
-    visit[T[0]] = new_set
+    head, tail = [0, 0], [0, 0]
+    visit[tail[0]] = {tail[1]}
     res = []
-    for step in steps:
-        dx, dy, x_diff, y_diff = step[0], step[1], H[0] - T[0], H[1] - T[1]
+    for dx, dy in steps:
+        x_diff, y_diff = head[0] - tail[0], head[1] - tail[1]
         if abs(x_diff + dx) == 2 or abs(y_diff + dy) == 2:
             if x_diff == 0 or y_diff == 0:
                 res.append([dx, dy])
-                T[0] += dx
-                T[1] += dy
+                tail[0] += dx
+                tail[1] += dy
             elif x_diff + dx == 0:
                 res.append([0, y_diff])
-                T[1] = H[1]
+                tail[1] = head[1]
             elif y_diff + dy == 0:
                 res.append([x_diff, 0])
-                T[0] = H[0]
+                tail[0] = head[0]
             else:
                 res.append([x_diff, y_diff])
-                T = H.copy()
-            new_set = visit.get(T[0], set())
-            new_set.add(T[1])
-            visit[T[0]] = new_set
-        H[0] += dx
-        H[1] += dy
+                tail = head.copy()
+            visit.setdefault(tail[0], set()).add(tail[1])
+        head[0] += dx
+        head[1] += dy
     return res
 
 def n_tails(steps, tail):
     for _ in range(tail):
         steps = move(steps)
-    count = 0
+    ct = 0
     for set in visit.values():
-        count += len(set)
-    return count
+        ct += len(set)
+    return ct
 
 def main():
     steps = []
     with open('../input/day9.txt') as file:
         for line in file.readlines():
-            direct, times = line.split(" ")
-            for _ in range(int(times)):
-                steps.append(directions[direct])
+            dir, time = line.strip().split(" ")
+            for _ in range(int(time)):
+                steps.append(direct[dir])
     print(n_tails(steps, 1))
     print(n_tails(steps, 9))
 
